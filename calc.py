75e7aa38145c2064f13899fa6a2cef5ddc5d5ad5
Sneha Mule <snehamule42@gmail.com>
2026-06-05 15:01:33 -0400

    tes1

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
-def add (a,b)
+def add (a,b):
     return a + b
-def subtract (a,b)
+def subtract (a,b):
     return a - b
-def multiply (a,b)
+def multiply (a,b):
     return a * b    
-def divide (a,b)   
+def divide (a,b): 
     return a / b
